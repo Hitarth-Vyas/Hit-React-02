7e210cf56ffb6cc1f746a4ec1dd82c2610a8ff34
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2021-11-23 09:13:54 +0530

    modified

---
+++ App.js
@@ -0,0 +1,19 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Navigate to='/quotes' />} />
+        <Route path='/quotes' element={<AllQuotes />} />
+        <Route path='/quotes/:quoteId/*' element={<QuoteDetail />} />
+        <Route path='/new-quote' element={<NewQuote />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ pages/QuoteDetail.js
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+import { useParams } from "react-router";
+import { Routes, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+
+const QuoteDetail = () => {
+  const params = useParams();
+
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+      <Routes>
+        <Route 
+          path="comments"
+          element={<Comments />}
+        />
+      </Routes>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
