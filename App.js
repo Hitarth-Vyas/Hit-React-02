3a68e81b472f62306de4a0cdf6719d6ad5aa31db
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2021-11-25 13:24:30 +0530

    router version 5.2.0

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Switch, Redirect } from 'react-router-dom';
+
 import AllQuotes from './pages/AllQuotes';
 import QuoteDetail from './pages/QuoteDetail';
 import NewQuote from './pages/NewQuote';
+import NotFound from './pages/NotFound';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Navigate to='/quotes' />} />
-        <Route path='/quotes' element={<AllQuotes />} />
-        <Route path='/quotes/:quoteId/*' element={<QuoteDetail />} />
-        <Route path='/new-quote' element={<NewQuote />} />
-      </Routes>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/quotes' />
+        </Route>
+        <Route path='/quotes' exact>
+          <AllQuotes />
+        </Route>
+        <Route path='/quotes/:quoteId'>
+          <QuoteDetail />
+        </Route>
+        <Route path='/new-quote'>
+          <NewQuote />
+        </Route>
+        <Route path='*'>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
